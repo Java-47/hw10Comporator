0ce77d908b4efdf2db0a1a8d3855b7bc40bfd965
kamen <kamen@DESKTOP-5HVTSJU.local>
2022-12-03 16:55:53 +0200

    hw10Comporator

---
+++ src/telran/comporator/OddEvenTest.java
@@ -0,0 +1,26 @@
+package telran.comporator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OddEvenTest {
+	Integer[] actual;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		actual = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 3 };
+
+	}
+
+	@Test
+	void test() {
+		Comparator<Integer> OddEvenComparator = new OddEvenComparator();
+		Arrays.sort(actual, OddEvenComparator);
+		Integer[] expected = new Integer[] { 2, 4, 6, 8, 9, 7, 5, 3, 3, 1 };
+		assertArrayEquals(expected, actual);
+	}
+}
